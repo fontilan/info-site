79c3d3c99922e28aff7485474cd92a78c520d307
fontilan <68748054+fontilan@users.noreply.github.com>
2022-01-21 15:45:48 +0100

    test

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
+import React from "react";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Header() {
+  return (
+    <header>
+      <nav className="nav">
+        <img className="nav-logo" src="logo192.png" alt="react logo" />
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function MainContent() {
+  return (
+    <div className="main-content">
+      <h1 className="bold">Reasons I'm excited to learn React</h1>
+      <ol>
+        <li>It's a popular framework, so I will fit in with the cool kids</li>
+        <li>
+          I will be more likely to find a job as a web developer knowing my way
+          around React
+        </li>
+        <li>
+          It actually looks like it can make building websites more managable
+          and easier to compatmentalize
+        </li>
+        <li>It's a new and exciting environment</li>
+      </ol>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <small>© 2022 Gmuzdek development. All rights reserved.</small>
+    </footer>
+  );
+}
+
+function Page() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+ReactDOM.render(<Page />, document.querySelector("#root"));
