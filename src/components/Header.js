1dca5762885a3234b2aa1c483a1a43d0d5cb1a0f
fontilan <68748054+fontilan@users.noreply.github.com>
2022-01-21 18:48:36 +0100

    remove unecessary header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,10 @@
 function Header() {
   return (
-    <header>
-      <nav>
-        <img src="logo192.png" alt="react logo" className="nav--logo" />
-        <h3 className="nav--logo_text">React Facts</h3>
-        <h4 className="nav--logo-title">React Course - Project 1</h4>
-      </nav>
-    </header>
+    <nav>
+      <img src="logo192.png" alt="react logo" className="nav--logo" />
+      <h3 className="nav--logo_text">React Facts</h3>
+      <h4 className="nav--logo-title">React Course - Project 1</h4>
+    </nav>
   );
 }
 export default Header;
