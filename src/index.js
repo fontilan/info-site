7d4ab6b1cdc3fa8e22b59fd703e29df8bc817cb3
fontilan <68748054+fontilan@users.noreply.github.com>
2022-01-21 18:35:55 +0100

    this is nice and clean now

--- src/index.js
+++ src/index.js
@@ -1,57 +1,8 @@
-import ReactDOM from "react-dom";
 import React from "react";
+import ReactDOM from "react-dom";
 
-function Header() {
-  return (
-    <header>
-      <nav className="nav">
-        <img className="nav-logo" src="logo192.png" alt="react logo" />
-        <ul className="nav-items">
-          <li>Pricing</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </header>
-  );
-}
+import App from "./App";
 
-function MainContent() {
-  return (
-    <div className="main-content">
-      <h1 className="bold">Reasons I'm excited to learn React</h1>
-      <ol>
-        <li>It's a popular framework, so I will fit in with the cool kids</li>
-        <li>
-          I will be more likely to find a job as a web developer knowing my way
-          around React
-        </li>
-        <li>
-          It actually looks like it can make building websites more managable
-          and easier to compatmentalize
-        </li>
-        <li>It's a new and exciting environment</li>
-      </ol>
-    </div>
-  );
-}
+import "./style.css";
 
-function Footer() {
-  return (
-    <footer className="footer">
-      <small>© 2022 Gmuzdek development. All rights reserved.</small>
-    </footer>
-  );
-}
-
-function Page() {
-  return (
-    <div>
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
-  );
-}
-
-ReactDOM.render(<Page />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector("#root"));
